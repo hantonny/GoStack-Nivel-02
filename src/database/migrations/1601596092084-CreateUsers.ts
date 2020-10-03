551f67e950ff90df4b6721c7cdb2aea19b807fb4
Hantonny Korrea <hantonny.20@gmail.com>
2020-10-02 20:30:59 -0400

    Cadastro de Usuários com Criptografia de Senha

--- src/database/migrations/1601596092084-CreateUsers.ts
+++ src/database/migrations/1601596092084-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1601596092084 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
