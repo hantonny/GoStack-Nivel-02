113c451380be5c138a3fb45bc84fb386ecaa2245
Hantonny Korrea <hantonny.20@gmail.com>
2020-10-02 20:25:48 -0400

    [200~

--- src/database/migrations/1601503152412-CreateAppointments.ts
+++ src/database/migrations/1601503152412-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1601503152412
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
